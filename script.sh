2a210880bcd15c009ac31d559513915a12de4fe7
Shreya Ghate <ghate.s@northeastern.edu>
2022-03-01 19:29:48 -0500

    Update: ami config

--- script.sh
+++ script.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+sleep 5
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-.home/shreya/.nvm/nvm.sh
-npm install -g npm
-npm install node
+.~/.nvm/nvm.sh 
+nvm install node 
+sudo yum install npm
+sudo yum install node
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo yum install pm2@latest 
 node -e \"console.log\('Running Node.js ' + process.version\)\"
 sudo yum -y update
 sudo tee /etc/yum.repos.d/pgdg.repo<<EOF
@@ -22,11 +26,19 @@ systemctl status postgresql-13
 #psql -c "ALTER USER ec2-user WITH SUPERUSER;"
 psql -c "ALTER USER shreya PASSWORD 'password';"
 
-sudo yum install -y unzip
-sudo which unzip
-sudo mkdir src 
-sudo cp webservice.zip src/
-sudo sh -c 'cd /src'
-sudo unzip webservice.zip 
-sudo sh -c 'cd /webservice'
+# sudo yum install -y unzip
+# sudo which unzip
+# sudo mkdir src 
+# sudo cp webservice.zip src/
+# sudo sh -c 'cd /src'
+# sudo unzip webservice.zip 
+# sudo sh -c 'cd /webservice'
+# sudo pm2 start app.js
+# sudo pm2 startup systemd
+# sudo pm2 save
+# sudo pm2 list
+
+sleep 10
+cd ~/webservice || exit
 sudo ls -a 
+node app.js
