11c8f30eb0254badc3ba82085a1b897c118c6d36
Shreya Ghate <ghate.s@northeastern.edu>
2022-03-31 14:42:21 -0400

    Update: A7 Final changes

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 sudo pm2 status
-echo "Stopping pm2 process"
 sudo pm2 stop all
-echo "Deleting pm2 process"
 sudo pm2 delete all
 sudo pm2 status
 
 cd /home/ec2-user/ || exit
 sudo rm -rf webservice node_modules 
-
-# echo "Before Install Step"
-# pwd
-# ls -al 
-# cd /home/ec2-user || exit
-# pwd 
-# echo "Stopping webservice"
-# sudo systemctl stop webservice
-# cd webservice/ || exit
-# sudo rm -rf node_modules
-# echo "Recreating webservice"
-# mkdir webservice
