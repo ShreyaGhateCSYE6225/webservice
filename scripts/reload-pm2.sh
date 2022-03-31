df6048c3ef74c01fc9319a6c5e9ef477c6d50486
Shreya Ghate <ghate.s@northeastern.edu>
2022-03-31 12:57:43 -0400

    Update: appspec

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-sudo systemctl start webservice
 cd /home/ec2-user/webservice || exit
-pm2 kill
-sudo npm install
-sudo rm -rf S3Config.js
-pm2 start app.js
-pm2 save
+sudo pm2 kill
+sudo pm2 start app.js
+
+# sudo systemctl start webservice
+# cd /home/ec2-user/webservice || exit
+# pm2 kill
+# sudo npm install
+# sudo rm -rf S3Config.js
+# pm2 start app.js
+# pm2 save
 # sudo pm2 reload all --update-env
