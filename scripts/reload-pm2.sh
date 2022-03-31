88b7e893473b4ddd929d26d75bff56fcc764f4fc
Shreya Ghate <ghate.s@northeastern.edu>
2022-03-31 13:57:32 -0400

    Update: appspec

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 cd /home/ec2-user/webservice || exit
-pwd
-ls
-pm2 stop all
-pm2 start app.js
-sudo pm2 startup systemd
-sudo pm2 save
-sudo ln -s /home/ec2-user/webservice/node-service.service /etc/systemd/system/node-service.service
-sudo systemctl daemon-reload
-sudo systemctl restart node-service.service
+sudo systemctl enable webservice
+sudo systemctl start webservice
+sudo pm2 reload all --update-env
+sudo pm2 start app.js
+if [ $? == 0 ]; then echo "Application started successfully"; else echo "Something went wrong while starting the application"; fi
+sudo pm2 status
+
+# cd /home/ec2-user/webservice || exit
+# pwd
+# ls
+# pm2 stop all
+# pm2 start app.js
+# sudo pm2 startup systemd
+# sudo pm2 save
+# sudo ln -s /home/ec2-user/webservice/node-service.service /etc/systemd/system/node-service.service
+# sudo systemctl daemon-reload
+# sudo systemctl restart node-service.service
 
 # sudo systemctl start webservice
 # cd /home/ec2-user/webservice || exit
