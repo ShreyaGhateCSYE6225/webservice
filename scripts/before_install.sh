b8cae58247b540ec0eaab355a0014891b31e3c39
Shreya Ghate <ghate.s@northeastern.edu>
2022-03-31 11:38:32 -0400

    Update: appspec

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-echo "Before Install Step"
-pwd
-ls -al 
-cd /home/ec2-user || exit
-pwd 
-echo "Stopping webservice"
-sudo systemctl stop webservice
-cd webservice/ || exit
-sudo rm -rf node_modules
+sudo pm2 status
+echo "Stopping pm2 process"
+sudo pm2 stop all
+echo "Deleting pm2 process"
+sudo pm2 delete all
+sudo pm2 status
+
+# echo "Before Install Step"
+# pwd
+# ls -al 
+# cd /home/ec2-user || exit
+# pwd 
+# echo "Stopping webservice"
+# sudo systemctl stop webservice
+# cd webservice/ || exit
+# sudo rm -rf node_modules
 # echo "Recreating webservice"
 # mkdir webservice
