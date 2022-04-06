804e712766745ee4e29d5be2b90124406706a8a6
Shreya Ghate <ghate.s@northeastern.edu>
2022-04-06 13:01:55 -0400

    update: Install CW script

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -2,10 +2,10 @@
 cd /home/ec2-user/webservice || exit
 sudo systemctl enable webservice
 sudo systemctl start webservice
-sudo pm2 reload all --update-env
-sudo pm2 start app.js
-if [ $? == 0 ]; then echo "Application has started successfully"; else echo "Something went wrong while starting the application"; fi
-sudo pm2 status
+# sudo pm2 reload all --update-env
+# sudo pm2 start app.js
+# if [ $? == 0 ]; then echo "Application has started successfully"; else echo "Something went wrong while starting the application"; fi
+# sudo pm2 status
 pm2 startOrReload ecosystem.config.js --name webservice
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
