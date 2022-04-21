ba05255fd1e2aba5ba08db6a97963eb0b5502f4d
Shreya Ghate <ghate.s@northeastern.edu>
2022-04-21 17:46:05 -0400

    Update: ssl

--- app/models/index.js
+++ app/models/index.js
@@ -1,16 +1,17 @@
 const dbconfigCreds = require("../config/db.config");
-
 const Sequelize = require("sequelize");
 const fs = require("fs");
 
 // const __dirname = null;
-const RDSCA = fs.readFileSync(process.env.PWD+'/app/models/us-east-1-bundle.pem');
+console.log("PWD", process.env.PWD);
+const RDSCA = fs.readFileSync(+'/app/models/us-east-1-bundle.pem');
+console.log("RDSCA", RDSCA);
 const sequelize = new Sequelize(dbconfigCreds.DB, dbconfigCreds.USER, dbconfigCreds.PASSWORD, {
     host: dbconfigCreds.HOST,
     port: dbconfigCreds.PORT,
     dialect: dbconfigCreds.dialect,
     ssl: {
-        // rejectUnauthorized: true,
+        rejectUnauthorized: true,
         ca: [RDSCA]
 },
     operatorAliases: false
